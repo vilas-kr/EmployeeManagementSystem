941703267fd9691aa4314bc9162993420962e4b9
vilas <vilaskr2962@gmail.com>
2025-12-11 12:34:22 +0530

    Present and absent check

--- src/main/java/com/bridgelabs/Main.java
+++ src/main/java/com/bridgelabs/Main.java
@@ -1,7 +1,17 @@
 package com.bridgelabs;
 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome to Employee Management System!");
+
+        int present = (int) (Math.random() * 10);
+
+        //any number below 5 is considered as absent else present
+        if(present < 5){
+            System.out.println("Employee absent");
+        }else{
+            System.out.println("Employee present");
+        }
     }
 }
