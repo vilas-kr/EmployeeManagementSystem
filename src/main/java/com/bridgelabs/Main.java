193e14ac7501f2e5fbf91d53d0d3c121e46f52ed
vilas <vilaskr2962@gmail.com>
2025-12-11 13:09:33 +0530

    Menu based interface added

--- src/main/java/com/bridgelabs/Main.java
+++ src/main/java/com/bridgelabs/Main.java
@@ -1,10 +1,43 @@
 package com.bridgelabs;
 
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome to Employee Management System!");
+        Scanner input = new Scanner(System.in);
 
+        while(true){
+            System.out.print("1. Check Attendance\n2. Calculate Full Time Wage\n3. Calculate Part Time Wage\n" +
+                    "4. exit\nEnter your choice: ");
+            int choice = input.nextInt();
+            switch(choice){
+                case 1:
+                    checkAttendance();
+                    break;
+
+                case 2:
+                    System.out.println("Daily wage for Full Time Employee is " + dailyEmployeeWage(8));
+                    break;
+
+                case 3:
+                    System.out.println("Daily wage for Part Time Employee is " + dailyEmployeeWage(4));
+                    break;
+
+                case 4: return;
+
+                default: System.out.println("Invalid choice");
+            }
+        }
+    }
+
+    public static int dailyEmployeeWage(int workHoursPerDay){
+        int wagePerHour = 20;
+        return wagePerHour * workHoursPerDay;
+    }
+
+    public static void checkAttendance(){
         int present = (int) (Math.random() * 10);
 
         //any number below 5 is considered as absent else present
@@ -13,19 +46,5 @@ public class Main {
         }else{
             System.out.println("Employee present");
         }
-
-
-        int workHoursPerDay = 8; //Full time employee
-        int wage = dailyEmployeeWage(workHoursPerDay);
-        System.out.println("Daily wage for Full Time Employee is " + wage);
-
-        workHoursPerDay = 4; //Part time employee
-        wage = dailyEmployeeWage(workHoursPerDay);
-        System.out.println("Daily wage for Part Time Employee is " + wage);
-    }
-
-    public static int dailyEmployeeWage(int workHoursPerDay){
-        int wagePerHour = 20;
-        return wagePerHour * workHoursPerDay;
     }
 }
